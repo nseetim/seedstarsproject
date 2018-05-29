a8de3fc8021197cc2181f8e1f40605800deeb282
Etim Nse-Abasi <etimnseabasi@gmail.com>
2018-05-29 10:37:18 +0100

    Added API's

--- HRapp/serializers.py
+++ HRapp/serializers.py
@@ -7,3 +7,5 @@ class AddDetailsSerializer(serializers.ModelSerializer):
     class Meta:
         model = AddDetails
         fields = ('username', 'emailaddress',)
+        # To get all the fields of the relation yoou can use
+        # fields = '__all__'
